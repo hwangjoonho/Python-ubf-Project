2246fb38e953506db8757bc71dee46c2afa583d3
stein5 <stein5@outlook.com>
2022-11-06 17:15:56 +0900

    remove example in models.py

--- galleries/models.py
+++ galleries/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 
-class Gallery(models.Model):
-    name = models.CharField(max_length=100)
-    # .......
+# class Gallery(models.Model):
+#     name = models.CharField(max_length=100)
+#     # .......
 
 
-class Picture(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    image = models.ImageField()
-    # .......
+# class Picture(models.Model):
+#     name = models.CharField(max_length=100)
+#     description = models.CharField(max_length=100)
+#     image = models.ImageField()
+#     # .......
