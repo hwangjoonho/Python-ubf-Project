31cffa1e16d90a148b6b3f0d03df6a41b5cbba9a
DESKTOP-N0F6QI9\raver <mary605@naver.com>
2022-11-20 17:28:50 +0900

    edit galleries - gallery filters updated

--- galleries/views.py
+++ galleries/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render
 def index(request):
     # 인덱스페이지에 들어갈 내용
     title = "사진첩"
+    span = "경성2부의 추억이 담긴 사진첩"
     return render(request, 'galleries/index.html', {
-        "page_title": title
+        "page_title": title,
+        "span" : span
     })
