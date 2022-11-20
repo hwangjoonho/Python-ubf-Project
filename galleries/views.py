ae9363840bba1b184fe3f753b22a8e8ba6eb1e4f
stein5 <stein5@outlook.com>
2022-11-20 16:05:34 +0900

    edit base, gallery-index page, add new base page

--- galleries/views.py
+++ galleries/views.py
@@ -5,4 +5,7 @@ from django.shortcuts import render
 # index 페이지
 def index(request):
     # 인덱스페이지에 들어갈 내용
-    return render(request, 'galleries/index.html')
+    title = "사진첩"
+    return render(request, 'galleries/index.html', {
+        "page_title": title
+    })
